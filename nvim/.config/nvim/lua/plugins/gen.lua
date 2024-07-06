787f98cf2a480cbcbe66430d29d182ae684161e4
marzeq <marzeqmarzeq@gmail.com>
2024-07-06 19:52:05 +0200

    nvim: allow changing gen ollama port and host using env vars

--- nvim/.config/nvim/lua/plugins/gen.lua
+++ nvim/.config/nvim/lua/plugins/gen.lua
@@ -3,6 +3,8 @@ return {
 		"David-Kunz/gen.nvim",
 		opts = {
 			display_mode = "split",
+      host = os.getenv("GEN_OLLAMA_HOST") or "localhost",
+      port = os.getenv("GEN_OLLAMA_PORT") or "11434"
 		},
 	},
 }
