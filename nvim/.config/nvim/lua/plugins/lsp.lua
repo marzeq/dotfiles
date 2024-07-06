4dc109be0b17e819be13dcb583d0fc2c62e1c24d
marzeq <marzeqmarzeq@gmail.com>
2024-07-06 17:16:31 +0200

    nvim: add autocomplete icons

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -29,7 +29,40 @@ return {
 
 	{
 		"nvimdev/epo.nvim",
-		opts = {},
+		dependencies = { { "onsails/lspkind.nvim", lazy = false } },
+		opts = {
+			kind_format = function(k)
+				local icons = {
+					Text = "󰉿",
+					Method = "󰆧",
+					Function = "󰊕",
+					Constructor = "",
+					Field = "󰜢",
+					Variable = "󰀫",
+					Class = "󰠱",
+					Interface = "",
+					Module = "",
+					Property = "󰜢",
+					Unit = "󰑭",
+					Value = "󰎠",
+					Enum = "",
+					Keyword = "󰌋",
+					Snippet = "",
+					Color = "󰏘",
+					File = "󰈙",
+					Reference = "󰈇",
+					Folder = "󰉋",
+					EnumMember = "",
+					Constant = "󰏿",
+					Struct = "󰙅",
+					Event = "",
+					Operator = "󰆕",
+					TypeParameter = "",
+				}
+
+				return icons[k]
+			end,
+		},
 	},
 
 	{
