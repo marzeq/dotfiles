1f30aaf69a4cc42232cbe0d7c622db155a09a323
marzeq <marzeqmarzeq@gmail.com>
2024-07-21 18:44:05 +0200

    nvim: update which-key config to match latest spec

--- nvim/.config/nvim/lua/config/remaps.lua
+++ nvim/.config/nvim/lua/config/remaps.lua
@@ -16,161 +16,154 @@ local function resizeToPercent(type, n)
 	vim.cmd(prefix .. "resize " .. new_width)
 end
 
-local function makeResizeRemap(type)
-	return {
-		name = type,
-
-		s = {
-			function()
-				resizeToPercent(type, 25)
-			end,
-			"Resize smaller (25%)",
-		},
-		b = {
-			function()
-				resizeToPercent(type, 75)
-			end,
-			"Resize bigger (75%)",
-		},
-		["="] = {
-			function()
-				resizeToPercent(type, 50)
-			end,
-			"Resize equal",
-		},
-		c = {
-			function()
-				vim.ui.input({ prompt = "Value for custom resize: " }, function(result)
-					local n = tonumber(result)
-
-					resizeToPercent(type, n)
-				end)
-			end,
-			"Resize custom",
-		},
-	}
-end
-
 local wk_remaps = {
+	{ "<leader>g", "<cmd>Format<cr>", desc = "Format current file" },
+
+	{ "<leader>a", group = "codeactions" },
+	{ "<leader>ah", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], desc = "Find and replace word" },
+	{ "<leader>ae", vim.diagnostic.open_float, desc = "Inspect error(s)" },
+	{ "<leader>ac", vim.diagnostic.goto_next, desc = "Cycle errors" },
+	{ "<leader>ar", vim.lsp.buf.rename, desc = "Rename symbol" },
+	{ "<leader>ad", "<cmd>Trouble diagnostics toggle<cr>", desc = "Project diagnostics" },
+	{ "<leader>aD", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "File diagnostics" },
+	{ "<leader>aa", vim.lsp.buf.code_action, desc = "Code action" },
+
+	{ "<leader>f", group = "file" },
 	{
-		mappings = {
-			f = {
-				name = "file",
+		"<leader>ff",
+		"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
+		desc = "Find file",
+	},
+	{ "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+	{ "<leader>fb", "<cmd>Telescope file_browser path=%:p:help |select_buffer=true<cr>|", desc = "File browser" },
+	{ "<leader>fw", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+	{ "<leader>fc", "<cmd>bd<cr>", desc = "Close current buffer" },
 
-				f = {
-					"<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>",
-					"Find file",
-				},
-				g = { "<cmd>Telescope live_grep<cr>", "Grep" },
-				b = { "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", "File browser" },
-				w = { "<cmd>Telescope buffers<cr>", "Buffers" },
-				c = { "<cmd>bd<cr>", "Close current buffer" },
-			},
-			l = {
-				name = "lsp",
+	{ "<leader>l", group = "lsp" },
+	{ "<leader>lm", "<cmd>Mason<cr>", desc = "Mason menu" },
+	{ "<leader>li", "<cmd>LspInfo<cr>", desc = "LSP info" },
 
-				m = { "<cmd>Mason<cr>", "Mason menu" },
-				i = { "<cmd>LspInfo<cr>", "LSP info" },
-			},
-			p = {
-				name = "plugins",
+	{ "<leader>p", group = "plugins" },
+	{ "<leader>pm", "<cmd>Lazy<cr>", desc = "Lazy menu" },
+	{ "<leader>ps", "<cmd>Lazy sync<cr>", desc = "Lazy sync" },
 
-				m = { "<cmd>Lazy<cr>", "Lazy menu" },
-				s = { "<cmd>Lazy sync<cr>", "Lazy sync" },
-			},
-			w = {
-				name = "window",
+	{ "<leader>t", group = "terminal" },
+	{
+		"<leader>th",
+		function()
+			vim.api.nvim_exec(
+				[[
+          split
+          wincmd j
+          term
+        ]],
+				false
+			)
 
-				v = { "<cmd>vsplit<cr><C-w>l", "Vertical split" },
-				h = { "<cmd>split<cr><C-w>j", "Horizontal split" },
-				r = {
-					name = "resize",
+			resizeToPercent("horizontal", 25)
 
-					v = makeResizeRemap("vertical"),
-					h = makeResizeRemap("horizontal"),
-				},
-			},
-			a = {
-				name = "codeactions",
+			vim.api.nvim_feedkeys("a", "n", true)
+		end,
+		desc = "Horizontal terminal",
+	},
+	{
+		"<leader>tv",
+		function()
+			vim.api.nvim_exec(
+				[[
+          vsplit
+          wincmd l
+          term
+        ]],
+				false
+			)
 
-				h = { [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], "Find and replace word" },
-				e = { vim.diagnostic.open_float, "Inspect error(s)" },
-				c = { vim.diagnostic.goto_next, "Cycle errors" },
-				r = { vim.lsp.buf.rename, "Rename symbol" },
-				D = { "<cmd>Trouble diagnostics toggle<cr>", "Project diagnostics" },
-				d = { "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", "File diagnostics" },
-				a = { vim.lsp.buf.code_action, "Code action" },
-			},
-			t = {
-				name = "terminal",
+			resizeToPercent("vertical", 25)
 
-				v = {
-					function()
-						vim.api.nvim_exec(
-							[[
-                vsplit
-                wincmd l
-                term
-              ]],
-							false
-						)
-
-						resizeToPercent("vertical", 25)
-
-						vim.api.nvim_feedkeys("a", "n", true)
-					end,
-					"Vertical terminal",
-				},
-				h = {
-					function()
-						vim.api.nvim_exec(
-							[[
-                split
-                wincmd j
-                term
-              ]],
-							false
-						)
-
-						resizeToPercent("horizontal", 25)
-
-						vim.api.nvim_feedkeys("a", "n", true)
-					end,
-					"Vertical terminal",
-				},
-			},
-			g = { "<cmd>Format<cr>", "Format current file" },
-		},
-		options = { prefix = "<leader>", mode = "n" },
+			vim.api.nvim_feedkeys("a", "n", true)
+		end,
+		desc = "Vertical terminal",
 	},
 
+	{ "<leader>w", group = "window" },
+	{ "<leader>wh", "<cmd>split<cr><C-w>j", desc = "Horizontal split" },
+	{ "<leader>wv", "<cmd>vsplit<cr><C-w>l", desc = "Vertical split" },
+	{ "<leader>wr", group = "resize" },
+	{ "<leader>wrh", group = "horizontal" },
 	{
-		mappings = {
-			P = { '"_dP', "Paste and keep buffer" },
-		},
-		options = { prefix = "<leader>", mode = "x" },
+		"<leader>wrh=",
+		function()
+			resizeToPercent("horizontal", 50)
+		end,
+		desc = "Resize equal",
+	},
+	{
+		"<leader>wrhb",
+		function()
+			resizeToPercent("horizontal", 75)
+		end,
+		desc = "Resize bigger (75%)",
+	},
+	{
+		"<leader>wrhs",
+		function()
+			resizeToPercent("horizontal", 25)
+		end,
+		desc = "Resize smaller (25%)",
+	},
+	{
+		"<leader>wrhc",
+		function()
+			vim.ui.input({ prompt = "Value for custom resize: " }, function(result)
+				local n = tonumber(result)
+
+				resizeToPercent("horizontal", n)
+			end)
+		end,
+		desc = "Resize custom",
+	},
+	{ "<leader>wrv", group = "vertical" },
+	{
+		"<leader>wrv=",
+		function()
+			resizeToPercent("vertical", 50)
+		end,
+		desc = "Resize equal",
+	},
+	{
+		"<leader>wrvb",
+		function()
+			resizeToPercent("vertical", 75)
+		end,
+		desc = "Resize bigger (75%)",
+	},
+	{
+		"<leader>wrvs",
+		function()
+			resizeToPercent("vertical", 25)
+		end,
+		desc = "Resize smaller (25%)",
+	},
+	{
+		"<leader>wrvc",
+		function()
+			vim.ui.input({ prompt = "Value for custom resize: " }, function(result)
+				local n = tonumber(result)
+
+				resizeToPercent("vertical", n)
+			end)
+		end,
+		desc = "Resize custom",
 	},
 
-	{
-		mappings = {
-			["<c-k>"] = { "<cmd>WhichKey<cr>", "Which Keys" },
-		},
-		options = {
-			mode = { "n", "x", "v" },
-		},
-	},
+	{ "<S-Tab>", "<cmd>BufferPrevious<cr>", desc = "Cycle buffers in reverse" },
+	{ "<Tab>", "<cmd>BufferNext<cr>", desc = "Cycle buffers" },
 
-	{ mappings = {
-		d = { vim.lsp.buf.definition, "Go to definition" },
-	}, options = { prefix = "g", mode = "n" } },
+	{ "<leader>P", '"_dP', desc = "Paste and keep buffer", mode = "x" },
 
-	{
-		mappings = {
-			["<Tab>"] = { "<cmd>BufferNext<cr>", "Cycle buffers" },
-			["<S-Tab>"] = { "<cmd>BufferPrevious<cr>", "Cycle buffers in reverse" },
-		},
-		options = { mode = "n" },
-	},
+	{ "<c-k>", "<cmd>WhichKey<cr>", desc = "Which Keys", mode = { "n", "v", "x" } },
+
+	{ "gd", vim.lsp.buf.definition, desc = "Go to definition" },
 }
 
 local remap = function()
--- nvim/.config/nvim/lua/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -8,12 +8,8 @@ return {
 
 			local wk = require("which-key")
 			require("config.remaps").remap()
-			local wk_remaps = require("config.remaps").wk_remaps
-
-			for i = 1, #wk_remaps do
-				wk.register(wk_remaps[i].mappings, wk_remaps[i].options)
-			end
 		end,
 		opts = {},
+    keys = require("config.remaps").wk_remaps
 	},
 }
