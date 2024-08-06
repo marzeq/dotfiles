f4df074260503c99547a2996fd905460311e367e
marzeq <marzeqmarzeq@gmail.com>
2024-08-06 10:40:49 +0200

    add mconf treesitter

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,28 +1,42 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter", -- better highlighting
-		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter", -- better highlighting
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
 
-			configs.setup({
-				ensure_installed = { "c", "lua", "vim", "rust", "vimdoc", "typescript", "javascript", "html" },
-				sync_install = false,
-				auto_install = true,
+      configs.setup({
+        ensure_installed = { "c", "lua", "vim", "rust", "vimdoc", "typescript", "javascript", "html" },
+        sync_install = false,
+        auto_install = true,
 
-				highlight = {
-					enable = true,
-				},
-				indent = {
-					enable = true,
-				},
-			})
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = true,
+        },
+      })
+
+      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+      parser_config.mconf = {
+        install_info = {
+          url = "https://github.com/marzeq/tree-sitter-mconf",
+          files = { "src/parser.c" },
+          branch = "dev",
+        },
+      }
+
+      vim.filetype.add({
+        -- *.mconf files
+        pattern = { [".*%.mconf"] = "mconf" },
+      })
 
       vim.filetype.add({
         pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
       })
-		end,
-	},
+    end,
+  },
 
 	-- lsp
 	{
