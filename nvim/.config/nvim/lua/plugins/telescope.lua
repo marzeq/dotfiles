7cc37b0375057804a053c12e0529f3d2d8a1ef02
marzeq <marzeqmarzeq@gmail.com>
2024-08-11 16:57:13 +0200

    nvim: customise file browser mappings

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,46 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep", "nvim-telescope/telescope-file-browser.nvim" },
-    opts = {
-      extensions = {
-        file_browser = {
-          hijack_netrw = true,
+    dependencies = { "nvim-lua/plenary.nvim", "BurntSushi/ripgrep" },
+    config = function()
+      local fb_actions = require("telescope._extensions.file_browser.actions")
+
+      require("telescope").setup({
+        extensions = {
+          file_browser = {
+            hijack_netrw = true,
+            initial_mode = "normal",
+            mappings = {
+              n = {
+                ["n"] = fb_actions.create,
+                ["r"] = fb_actions.rename,
+                ["m"] = fb_actions.move,
+                ["c"] = fb_actions.copy,
+                ["d"] = fb_actions.remove,
+                ["x"] = fb_actions.open,
+                ["g"] = fb_actions.goto_parent_dir,
+                ["`"] = fb_actions.goto_home_dir,
+                ["-"] = fb_actions.goto_cwd,
+                ["t"] = fb_actions.change_cwd,
+                ["f"] = fb_actions.toggle_browser,
+                ["h"] = fb_actions.toggle_hidden,
+                ["s"] = fb_actions.toggle_all,
+              },
+            },
+          },
+          ["ui-select"] = require("telescope.themes").get_dropdown({}),
         },
-        ["ui-select"] = require("telescope.themes").get_dropdown({}),
-      },
-      defaults = {
-        sorting_strategy = "ascending",
-        layout_config = {
-          horizontal = {
-            prompt_position = "top",
-            preview_width = 0.3,
+        defaults = {
+          sorting_strategy = "ascending",
+          layout_config = {
+            horizontal = {
+              prompt_position = "top",
+              preview_width = 0.3,
+            },
           },
         },
-      },
-    },
-    config = function(_, opts)
-      require("telescope").setup(opts)
+      })
+
       require("telescope").load_extension("file_browser")
       require("telescope").load_extension("ui-select")
     end,
@@ -28,5 +48,9 @@ return {
 
   {
     "nvim-telescope/telescope-ui-select.nvim",
+  },
+
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
   },
 }
