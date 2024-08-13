fd9bbb99d4c62b54b88139bc7a2852ee4abb21d6
marzeq <marzeqmarzeq@gmail.com>
2024-08-13 18:40:10 +0200

    nvim: add leetcode.nvim plugin

---
+++ nvim/.config/nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      lang = "python3",
+    },
+  },
+}
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    lazy = false,
     config = function()
       local configs = require("nvim-treesitter.configs")
 
