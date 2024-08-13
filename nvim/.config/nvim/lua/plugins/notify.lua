7091b2262067be2b1672cd73ee1eca8a10af4b99
marzeq <marzeqmarzeq@gmail.com>
2024-08-13 17:20:07 +0200

    nvim: add nvim-notify

---
+++ nvim/.config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#000000",
+    },
+    config = function(_, opts)
+      require("notify").setup(opts)
+      vim.notify = require("notify")
+    end,
+  },
+}
