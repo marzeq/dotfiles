7c62d3b2def5bedfe918f0001f7656744ba32b76
marzeq <marzeqmarzeq@gmail.com>
2024-09-10 21:27:48 +0200

    nvim: change default lsps

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -7,7 +7,7 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = {
-      ensure_installed = { "lua_ls", "rust_analyzer", "tsserver" },
+      ensure_installed = { "lua_ls", "rust_analyzer", "gopls" },
       handlers = {
         function(server_name)
           local capabilities =
