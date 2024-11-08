1dadbfecf73868a75c5ece7903467e110c69b454
marzeq <marzeqmarzeq@gmail.com>
2024-11-08 15:54:46 +0100

    .

--- hypr/.config/hypr/scripts/powermenu.sh
+++ hypr/.config/hypr/scripts/powermenu.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-MODE=$(echo "suspend
+MODE=$(echo "sleep
 logout
 reboot
-poweroff" | rofi -dmenu -p "Power menu")
+shutdown" | rofi -dmenu -p "Power menu")
 
 if [[ ! -z "$MODE" ]]; then
   if [ $MODE == "logout" ]; then
     hyprctl dispatch exit
-  else
-    systemctl $MODE
+  elif [ $MODE == "reboot" ]; then
+    systemctl reboot
+  elif [ $MODE == "shutdown" ]; then
+    systemctl poweroff
+  elif [ $MODE == "sleep" ]; then
+    systemctl suspend
   fi
 fi
