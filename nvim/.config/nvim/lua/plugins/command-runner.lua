aa59be16d2cfdfedb49f765130db03930da48d35
marzeq <marzeqmarzeq@gmail.com>
2025-02-24 13:16:03 +0100

    .

--- nvim/.config/nvim/lua/plugins/command-runner.lua
+++ nvim/.config/nvim/lua/plugins/command-runner.lua
@@ -3,8 +3,7 @@ return {
     -- dir = "~/code/command-runner.nvim",
     "marzeq/command-runner.nvim",
     opts = {
-      backend = "redr",
-      redr_show_could_not_connect = false,
+      backend = "native",
     },
   },
 }
