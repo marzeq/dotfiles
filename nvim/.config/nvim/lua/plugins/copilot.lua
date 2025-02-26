de7132a6b3b246014b503a3cd595c030966a096f
marzeq <marzeqmarzeq@gmail.com>
2025-02-26 14:00:29 +0100

    .

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,5 +1,19 @@
 return {
   {
-    "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
+    lazy = false,
+    opts = {
+      panel = {
+        enabled = false,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = false,
+        keymap = {
+          accept = "<Tab>",
+        },
+      },
+    },
   },
 }
