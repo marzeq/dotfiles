b67b266fe172f7a3797eefeae59e7ec905e8de22
Antoni Marzec <amarzec@outlook.com>
2025-09-03 19:00:54 +0200

    nvim: snacks never open qflist immediately

--- nvim/.config/nvim/lua/plugins/snacks-nvim.lua
+++ nvim/.config/nvim/lua/plugins/snacks-nvim.lua
@@ -25,7 +25,7 @@ return {
         ui = {
           selected = "+ ",
           unselected = " ",
-        }
+        },
       },
     },
     image = {},
@@ -38,6 +38,39 @@ return {
       scope = { enabled = false },
     },
   },
+  config = function (_, opts)
+    require("snacks").setup(opts)
+
+    local function setqflist(items)
+      local qf = {}
+      for _, item in ipairs(items) do
+        qf[#qf + 1] = {
+          filename = Snacks.picker.util.path(item),
+          bufnr = item.buf,
+          lnum = item.pos and item.pos[1] or 1,
+          col = item.pos and item.pos[2] + 1 or 1,
+          end_lnum = item.end_pos and item.end_pos[1] or nil,
+          end_col = item.end_pos and item.end_pos[2] + 1 or nil,
+          text = item.line or item.comment or item.label or item.name or item.detail or item.text,
+          pattern = item.search,
+          valid = true,
+        }
+      end
+      vim.fn.setqflist(qf)
+    end
+
+    Snacks.picker.actions.qflist = function (picker)
+      picker:close()
+      local sel = picker:selected()
+      local items = #sel > 0 and sel or picker:items()
+      setqflist(items)
+    end
+
+    Snacks.picker.actions.qflist_all = function (picker)
+      picker:close()
+      setqflist(picker:items())
+    end
+  end,
   keys = {
     { "<leader>ff", function () Snacks.picker.files() end, desc = "Find files" },
     { "<leader>fi", function () Snacks.picker.files({ ignored = true }) end, desc = "Find ignored files" },
