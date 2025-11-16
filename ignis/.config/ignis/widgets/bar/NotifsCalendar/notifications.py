7e9ac059cc4aa2837d79b79d8291742eb7a57b10
Antoni Marzec <amarzec@outlook.com>
2025-11-17 00:46:10 +0100

    ignis: music player in notifications area

--- ignis/.config/ignis/widgets/bar/NotifsCalendar/notifications.py
+++ ignis/.config/ignis/widgets/bar/NotifsCalendar/notifications.py
@@ -1,11 +1,79 @@
+from typing import Callable
 from ignis.services.notifications import NotificationService
+from ignis.services.mpris import MprisPlayer, MprisService
 from ignis.utils import Utils
 from ignis.widgets import Widget
-
 from widgets.shared.Notification import NotificationWidget
 
-
+mpris = MprisService.get_default()
 notifications = NotificationService.get_default()
+
+class PlayerControlButton(Widget.Button):
+    def __init__(self, icon: str, on_click: Callable[[], None], enabled: bool = True):
+        super().__init__(
+            child=Widget.Icon(
+                image=icon,
+                pixel_size=16,
+                css_classes=["nc-player-control-icon"] + (["disabled"] if not enabled else [])
+            ),
+            css_classes=["nc-player-control-button"] + (["disabled"] if not enabled else []),
+            on_click=lambda *_: on_click(),
+            sensitive=enabled,
+        )
+
+class PlayerWidget(Widget.CenterBox):
+    def __init__(self, player: MprisPlayer):
+        super().__init__(
+            css_classes=["nc-player"],
+            start_widget=Widget.Box(
+                child=[
+                    Widget.Icon(
+                        image=player.art_url,
+                        css_classes=["nc-player-icon"],
+                        pixel_size=48,
+                        visible=bool(player.art_url)
+                    ),
+                    Widget.Box(
+                        vertical=True,
+                        valign="center",
+                        child=[
+                            Widget.Label(
+                                label=player.title,
+                                css_classes=["nc-player-title"],
+                                halign="start",
+                                ellipsize="end",
+                            ),
+                            Widget.Label(
+                                label=player.artist,
+                                css_classes=["nc-player-artist"],
+                                halign="start",
+                                ellipsize="end",
+                            ),
+                        ],
+                    ),
+                ]
+            ),
+            end_widget=Widget.Box(
+                css_classes=["nc-player-controls"],
+                child=[
+                    PlayerControlButton(
+                        icon="media-skip-backward-symbolic",
+                        on_click=lambda: player.previous(),
+                        enabled=player.bind("can_go_previous"),
+                    ),
+                    PlayerControlButton(
+                        icon=player.bind("playback_status", lambda s: "media-playback-pause-symbolic" if s == "Playing" else "media-playback-start-symbolic"),
+                        on_click=lambda: player.play_pause(),
+                    ),
+                    PlayerControlButton(
+                        icon="media-skip-forward-symbolic",
+                        on_click=lambda: player.next(),
+                        enabled=player.bind("can_go_next"),
+                    ),
+                ]
+            )
+        )
+
 
 class Notifications(Widget.Box):
     def __init__(self):
@@ -15,6 +83,8 @@ class Notifications(Widget.Box):
         self._update_body()
 
         notifications.connect("notify::notifications", lambda *_: self._update_body())
+        mpris.connect("notify::players", lambda *_: self._update_body())
+
         Utils.Poll(60_000, lambda *_: self._update_body()) # to update the "X minutes ago" etc. labels
 
     def _update_body(self):
@@ -49,10 +119,10 @@ class Notifications(Widget.Box):
                         ),
                     ],
                 )
-            ] if not notifs else [Widget.Scroll(
+            ] if not notifs and not mpris.players else [Widget.Scroll(
                 child=Widget.Box(
                     vertical=True,
-                    child=[NotificationWidget(n, show_time=True) for n in notifs],
+                    child=[PlayerWidget(p) for p in mpris.players] + [NotificationWidget(n, show_time=True) for n in notifs],
                 ),
                 css_classes=["nc-notifications-scroll"],
             )],
