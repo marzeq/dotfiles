6b5a03dc028a3742c2be3b7e4702bbf327edba9c
Antoni Marzec <amarzec@outlook.com>
2025-11-17 11:22:17 +0100

    begone rofi and waybar, youve been here for too long

--- waybar/.config/waybar/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-paru -Syu --noconfirm
-orphans=$(pacman -Qdtq)
-if [ -n "$orphans" ]; then
-  sudo pacman -Rns $(pacman -Qdtq) --noconfirm
-fi
--- waybar/.config/waybar/update_count.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CHK=$(checkupdates | wc -l)
-CHKAUR=$(paru -Qau | wc -l)
-SUM=$(($CHK + $CHKAUR))
-
-
-if ! [ "$SUM" -eq 0 ]; then
-  echo ${SUM}
-fi
