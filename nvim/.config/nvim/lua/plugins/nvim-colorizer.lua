1e0728bd08c2a3467728bee7bc7b90b6463eb52e
Antoni Marzec <amarzec@outlook.com>
2026-04-13 12:41:23 +0200

    .

--- nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
+++ nvim/.config/nvim/lua/plugins/nvim-colorizer.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "norcalli/nvim-colorizer.lua",
+    "catgoose/nvim-colorizer.lua",
     event = { "BufReadPre", "BufNewFile" },
     init = function()
       vim.o.termguicolors = true
