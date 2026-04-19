7779995e9afb4deb3607826941faf410ecfa6b89
Antoni Marzec <amarzec@outlook.com>
2026-04-19 11:06:12 +0200

    feat: update nvim-treesitter configuration

--- nvim/.config/nvim/lua/plugins/languages.lua
+++ nvim/.config/nvim/lua/plugins/languages.lua
@@ -8,23 +8,7 @@ return {
   {
     "marzeq/tree-sitter-mconf",
     config = function()
-      require("nvim-treesitter.parsers").mconf = {
-        install_info = {
-          url = "https://github.com/marzeq/tree-sitter-mconf",
-          files = { "src/parser.c" },
-        },
-      }
-
-      vim.filetype.add({
-        pattern = { [".*%.mconf"] = "mconf" },
-      })
-
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "mconf",
-        callback = function()
-          vim.bo.commentstring = "# %s"
-        end,
-      })
+      
     end,
   },
 }
--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -10,24 +10,39 @@ return {
       auto_install = true,
     },
     init = function()
+      vim.filetype.add({
+        pattern = { [".*%.mconf"] = "mconf" },
+      })
+
+      vim.api.nvim_create_autocmd("User", { pattern = "TSUpdate",
+      callback = function()
+        require("nvim-treesitter.parsers").mconf = {
+          install_info = {
+            url = "https://github.com/marzeq/tree-sitter-mconf",
+            revision = "f1422fe2c06c6e7f7b7ba3b48bb26364aef5fec7",
+            queries = "queries/mconf",
+          },
+          tier = 2,
+        }
+      end})
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "mconf",
+        callback = function()
+          vim.bo.commentstring = "# %s"
+        end,
+      })
+
       vim.api.nvim_create_autocmd("FileType", {
         callback = function(ev)
           local lang = vim.treesitter.language.get_lang(ev.match)
           local available_langs = require("nvim-treesitter").get_available()
           local is_available = vim.tbl_contains(available_langs, lang)
           if is_available then
-            local installed_langs = require("nvim-treesitter").get_installed()
-            local installed = vim.tbl_contains(installed_langs, lang)
-            if not installed then
-              require("nvim-treesitter").install(lang):await(function ()
-                vim.notify("Installed " .. lang .. " parser", vim.log.levels.INFO, { title = "nvim-treesitter" })
-                vim.treesitter.start()
-                require("nvim-treesitter").indentexpr()
-              end)
-            else
+            require("nvim-treesitter").install(lang):await(function ()
               vim.treesitter.start()
               require("nvim-treesitter").indentexpr()
-            end
+            end)
           end
         end,
       })
